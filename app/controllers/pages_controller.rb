6aca282ba527cb1325a81feca1095c25d449ac39
luiz fernando sales <lnandofonseca@gmail.com>
2024-01-11 12:45:34 -0300

    commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,25 @@ class PagesController < ApplicationController
   def home
     @home_page = true
   end
+
+  def services
+    @services_page = true
+  end
+
+  def areas
+    @areas = true
+  end
+
+  def office
+    @office = true
+  end
+
+  def blog
+    @blog = true
+  end
+
+  def contact
+    @contact = true
+    
+  end
 end
